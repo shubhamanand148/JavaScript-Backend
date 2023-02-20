2e3552c4d59da001f23204764ea712c8cb838de5
Shubham Anand <shubhamanand148@gmail.com>
2023-02-21 02:27:30 +0530

    JavaScript

---
+++ DrumKit/index.js
@@ -0,0 +1,54 @@
+//This will get the number of elements which have the drum class. i.e. the number of drum elements.
+var NumOfDrums = document.querySelectorAll(".drum").length;
+
+// This will loop through all the drum elements.
+for (var i=0; i<NumOfDrums; i++){
+
+    // This will get the element which has the class drum and we loop through all such elements.
+    // Then we add an event listener to those drum elements which will call the function
+    // when the "drum" class element is clicked.
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+
+        var drum_pressed = this.innerHTML;
+
+        switch (drum_pressed) {
+            case "w":
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+
+            case "s":
+                var audio = new Audio("sounds/tom-2.mp3");
+                audio.play();
+                break;
+
+            case "a":
+                var audio = new Audio("sounds/tom-3.mp3");
+                audio.play();
+                break;
+
+            case "d":
+                var audio = new Audio("sounds/tom-4.mp3");
+                audio.play();
+                break;
+
+            case "j":
+                var audio = new Audio("sounds/snare.mp3");
+                audio.play();
+                break;
+
+            case "k":
+                var audio = new Audio("sounds/kick-bass.mp3");
+                audio.play();
+                break;
+
+            case "l":
+                var audio = new Audio("sounds/crash.mp3");
+                audio.play();
+                break;
+
+            default:
+                console.log(drum_pressed);
+        }
+    });
+}
