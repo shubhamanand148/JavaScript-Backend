1780fe19f26e71435417c24b129867c70dec80e2
Shubham Anand <shubhamanand148@gmail.com>
2023-03-14 23:52:39 +0530

    JavaScript

--- EmailSubscriber/signUp.js
+++ EmailSubscriber/signUp.js
@@ -1,14 +1,18 @@
+// const API_KEY = "36b7fc1e272e470b75ffee489af06112-us21";
+// const AUDIENCE_KEY = "a3baffc06b";  
+// const URL = "https://us21.api.mailchimp.com/3.0/lists/" + AUDIENCE_KEY;
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const https = require('https');
+const request = require("request");
 
 const app = express();
 app.use(express.static("static"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-
 // This will run the website on a random port on Heroku server or port 3000 in localhost.
-app.listen(precess.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Session started");
 });
 
@@ -34,9 +38,9 @@ app.post("/", function(req, res){
             }
         }]
     };
-    
+
     const jsonData = JSON.stringify(user);
-    const API_KEY = "dbbc08d63f214412189e14745bc5244-us21";
+    const API_KEY = "36b7fc1e272e470b75ffee489af06112-us21";
     const AUDIENCE_KEY = "a3baffc06b";
     
     const URL = "https://us21.api.mailchimp.com/3.0/lists/" + AUDIENCE_KEY;
@@ -45,23 +49,26 @@ app.post("/", function(req, res){
         method: "POST",
         auth: "anything:" + API_KEY
     }
-    
+
+    var heading = "", description = "";
+
     const request_data = https.request(URL, options, function(response){
 
-        if(response.statusCode == 200){
+        const status_code = response.statusCode;
+        console.log(status_code);
+        if(status_code == 200){
             res.sendFile(__dirname + "/Signin Success.html");
         } else{
             res.sendFile(__dirname + "/Signin Failure.html");
+
         }
-        response.on("data", function(user){
-            console.log(response.statusCode);
-        })
     });
     
-    request_data. write(jsonData);
+    request_data.write(jsonData);
     request_data.end();
+    //request_data.render("Signin Failure", {title: heading})
 });
 
 app.post("/failure", function(req, res){
-    res.redirect("/")
+    res.redirect("/");
 })
